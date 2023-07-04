1b9c8b70acba03e6593dd2c445eeb8442c70c1f0
Pre-11 <prernakumari7427@gmail.com>
2023-07-04 22:57:52 +0530

    added landing page

--- education.js
+++ education.js
@@ -1,12 +1,31 @@
 
 
-function createCard(img1,img2,title,text){
-    let contain= document.getElementById("divs");
+function createCard(img1,img2,title,texts){
+    
     let container = document.createElement("div");
-    let image1=document.createElement("img");
-    image1.src = "Image/heart.png";
-    container.appendChild(image1);
-    contain.append(container);
+    container.className = "Container";
 
+    let image1=document.createElement("img");
+    image1.src = (img1);
+
+    let image2=document.createElement("img");
+    image2.src = (img2);
+
+    let tit = document.createElement("h4");
+    tit.innerHTML=(title);
+
+    let text = document.createElement("h5");
+    text.innerHTML=(texts); 
+
+   
+    let box = document.querySelector(".card-container");
+
+    container.append(image1,image2,tit,text);
+    box.append(container);
+
+   
 }
-createCard();
+
+
+
+let card1 = createCard('Image/icon1.png','Image/circle1.png',"Certificate"," certificate that an educator has them to perform a task requiring. has them to perform a task requiring ");
