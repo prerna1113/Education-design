c7a376072f48e9ff3154ac8d965d521524fdcc1c
Pre-11 <prernakumari7427@gmail.com>
2023-07-04 21:49:38 +0530

    icon added

--- education.js
+++ education.js
@@ -1,43 +1,12 @@
 
 
+function createCard(img1,img2,title,text){
+    let contain= document.getElementById("divs");
+    let container = document.createElement("div");
+    let image1=document.createElement("img");
+    image1.src = "Image/heart.png";
+    container.appendChild(image1);
+    contain.append(container);
 
-const cardsData = [
-    
-    {
-      imageSrc: 'Image/image2.png',
-      title: 'Card 2',
-      description: 'Description for Card 2',
-    },
-    {
-      imageSrc: 'Image/image3.png',
-      title: 'Card 3',
-      description: 'Description for Card 3',
-    },
-  ];
-  const cardContainer = document.getElementsByClassName('cards');
-  for (const cardData of cardsData) {
-    const card = document.createElement('div');
-    card.className = 'card';
-    
-    const image = document.createElement('img');
-    image.src = cardData.imageSrc;
-    image.className = 'card-img-top';
-    image.alt = 'Image';
-    card.appendChild(image);
-    
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-    
-    const title = document.createElement('h5');
-    title.className = 'form-control';
-    title.textContent = cardData.title;
-    cardBody.appendChild(title);
-    
-    const description = document.createElement('p');
-    description.className = 'card-text';
-    description.textContent = cardData.description;
-    cardBody.appendChild(description);
-      
-    card.append(cardBody);
-    cardContainer.appendChild(card);
-  }
+}
+createCard();
---
+++ try.js
@@ -0,0 +1,5 @@
+
+let heading = document.getElementById('container');
+let tag=document.createElement('div');
+tag.innerHTML = "Hello";
+heading.appendChild(tag);
