7e282154ebc9aff0da60221235708ebc43cfcf18
Pre-11 <prernakumari7427@gmail.com>
2023-07-05 11:23:34 +0530

    another page added

--- education.js
+++ education.js
@@ -39,6 +39,43 @@ box.appendChild(sndContainer);
 
 let card1 = createCard('Image/icon1.png','Image/circle1.png',"Certificate"," certificate that an educator has <br> them to perform a task requiring. <br>has them to perform a task requiring.<br> ");
 
-let card2 = createCard('Image/heart.png',"Image/circle2.png","Life Program","Life Programer educator has them <br> to perform a task requiring. <br>has them to perform a task requiring");
+let card2 = createCard('Image/heart.png',"Image/circle2.png","Life Programs","Life Programer educator has them <br> to perform a task requiring. <br>has them to perform a task requiring");
 
 // let card3 = createCard('Image/icon3.png',"Image/circle3.png","Life Program","Life Programer educator has them <br> to perform a task requiring. <br>has them to perform a task requiring");
+
+const FContent = document.querySelector('.scndCard-Container');
+
+const h5Tag = document.createElement('h5');
+h5Tag.innerHTML = "Why Choose Us ?";
+h5Tag.className = "h5-tag";
+
+const MContent = document.querySelector('.MContent');
+
+
+const h1Tag = document.createElement("h1");
+h1Tag.innerHTML = "Our Education System <br>  Will Give You Solution"
+h1Tag.className = "h1-tag";
+
+const h6Tag = document.createElement( 'h6');
+h6Tag.innerHTML = "we Provide a comprehensive range of data protection and cyber <br> security servies to protect your buisness infromation  and to <br>give you a piece-of-mind";
+h6Tag.className = "h6-tag";
+
+const h7Tag = document.createElement( 'h6');
+h7Tag.innerHTML = "we Provide a comprehensive range of data protection and cyber <br> security servies to protect your buisness infromation  and to <br>give you a piece-of-mind";
+h7Tag.className = "h7-tag";
+
+FContent.append(h5Tag);
+
+MContent.append(h1Tag,h6Tag,h7Tag);
+let Card3 = document.querySelector('.d');
+
+// let card3 = createCard('Image/icon1.png','Image/circle1.png',"Certificate"," certificate that an educator has <br> them to perform a task requiring. <br>has them to perform a task requiring.<br> ");
+
+// let card4 = createCard('Image/heart.png',"Image/circle2.png","Life Programs","Life Programer educator has them <br> to perform a task requiring. <br>has them to perform a task requiring");
+
+
+//  Card3.append(card3,card4);
+
+
+
+
